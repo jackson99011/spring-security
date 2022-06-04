1730f538a20f540f75e9acd76eed143cdce7d3d5
Eazy Bytes <tutor@eazybytes.com>
2022-06-04 11:49:09 +0530

    Custom Security Configurations, PermitAll, DenyAll Configurations

--- section2/springsecsection2latest/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ section2/springsecsection2latest/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,74 +2,57 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig  {
 
 	/**
-	 *
+	 * From Spring Security 5.7, the WebSecurityConfigurerAdapter is deprecated to encourage users
+	 * to move towards a component-based security configuration. It is recommended to create a bean
+	 * of type SecurityFilterChain for security related configurations.
 	 * @param http
 	 * @return
 	 * @throws Exception
 	 */
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)http.authorizeRequests().anyRequest()).
+
+		/**
+		 * Default configurations which will secure all the requests
+		 */
+		/*((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)http.authorizeRequests().anyRequest()).
 				authenticated();
 		http.formLogin();
 		http.httpBasic();
-		return (SecurityFilterChain)http.build();
-	}
+		return (SecurityFilterChain)http.build();*/
 
-	/**
-	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
-	 * Secured /notices - Not Secured /contact - Not Secured
-	 *//*
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		*//**
-		 * Default configurations which will secure all the requests
-		 *//*
-
-		*//*
-		 * http .authorizeRequests() .anyRequest().authenticated() .and()
-		 * .formLogin().and() .httpBasic();
-		 *//*
-
-		*//**
+		/**
 		 * Custom configurations as per our requirement
-		 *//*
+		 */
+		http.authorizeHttpRequests( (auth)->auth
+				.antMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+				.antMatchers("/notices","/contact").permitAll()
+		).httpBasic(Customizer.withDefaults());
+		return http.build();
 
-		*//*
-		 * http .authorizeRequests() .antMatchers("/myAccount").authenticated()
-		 * .antMatchers("/myBalance").authenticated()
-		 * .antMatchers("/myLoans").authenticated()
-		 * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
-		 * .antMatchers("/contact").permitAll() .and() .formLogin().and() .httpBasic();
-		 *//*
-
-		*//**
+		/**
 		 * Configuration to deny all the requests
-		 *//*
+		 */
+		/*http.authorizeHttpRequests( (auth)->auth
+				.anyRequest().denyAll())
+				.httpBasic(Customizer.withDefaults());
+		return http.build();*/
 
-		*//*
-		 * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
-		 * .httpBasic();
-		 *//*
-
-		*//**
+		/**
 		 * Configuration to permit all the requests
-		 *//*
-
-		http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
-		 .httpBasic();
-		
-
-	}*/
+		 */
+		/*http.authorizeHttpRequests( (auth)->auth
+						.anyRequest().permitAll())
+				.httpBasic(Customizer.withDefaults());
+		return http.build();*/
+	}
 
 }
